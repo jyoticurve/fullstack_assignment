b6e39c264187ffeb4c3bea1b49a37d5bcdab8a6c
jyoticurve <142990553+jyoticurve@users.noreply.github.com>
2023-12-13 14:50:36 +0530

    Add files via upload
    Today I complete and submit my both Javascript operation assignment and Javascript and its fundamental assignment which are combined in javascript fundamental assignment of 4th nov'23

---
+++ Javascript fundamental assignment - 1/Javascript operation assignment/7th solution/script.js
@@ -0,0 +1,7 @@
+const radius = 10; // Radius of the circle
+
+// Calculate the area of the circle: area = pi * radius^2
+const pi = Math.PI;   
+const area = pi * Math.pow(radius, 2);
+
+console.log("Area of the circle:", area);
